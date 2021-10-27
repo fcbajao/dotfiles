5be1e8cdfe54d86b6545c9d959194da475294ee7
Erick Bajao <fbajao@gitlab.com>
2021-10-27 13:42:38 +0000

    Add docker specific lines

--- setup.sh
+++ setup.sh
@@ -160,6 +160,10 @@ if ! [ -x "$(command -v docker)" ]; then
     echo "Installing docker compose..."
     sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
     sudo chmod +x /usr/local/bin/docker-compose
+
+    # https://stackoverflow.com/a/48957722
+    sudo usermod -aG docker $USER
+    newgrp docker
   fi
 else
   echo "Command docker found, skipping install..."
