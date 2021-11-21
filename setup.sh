86869279db8816fe736534e4e3d4aa05f2c5e9e6
Erick Bajao <fbajao@gitlab.com>
2021-11-21 16:09:24 +0000

    Add setup for fzf

--- setup.sh
+++ setup.sh
@@ -137,6 +137,14 @@ else
   sudo apt-get install -y silversearcher-ag
 fi
 
+if ! [ -x "$(command -v fzf)" ]; then
+  echo "Installing fzf..."
+  git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+  ~/.fzf/install
+else
+  echo "Command fzf found, skipping..."
+fi
+
 if ! [ -x "$(command -v docker)" ]; then
   if ! [ -x "$(command -v apt-get)" ]; then
     echo "No apt-get found, assuming this is OSX."
